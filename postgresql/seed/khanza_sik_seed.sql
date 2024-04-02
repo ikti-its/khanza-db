3e1c23c6f55a1fda20b79ede143c75b5829fa825
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-02 13:15:11 +0700

    chore: update default akun foto url

--- postgresql/seed/khanza_sik_seed.sql
+++ postgresql/seed/khanza_sik_seed.sql
@@ -1,4 +1,4 @@
 -- Akun
 INSERT INTO akun (email, password, foto, role) VALUES
-('admin@fathoor.dev', '$2a$10$8jI.qKrVbXQjNzYX6KOIvukkYkNcmfYyPWiv9tuHE8vdg5EhjQBzy', 'http://localhost:8080/v1/file/img/default.png', 1),
-('fathoor@fathoor.dev', '$2a$10$Yg78XjsfHtvpiZHzxjhtBeauNRpK928c1zKSXjPdV.jOE0q21qXgq', 'http://localhost:8080/v1/file/img/default.png', 1337);
+('admin@fathoor.dev', '$2a$10$8jI.qKrVbXQjNzYX6KOIvukkYkNcmfYyPWiv9tuHE8vdg5EhjQBzy', 'https://api.fathoor.dev/v1/file/img/default.png', 1),
+('fathoor@fathoor.dev', '$2a$10$Yg78XjsfHtvpiZHzxjhtBeauNRpK928c1zKSXjPdV.jOE0q21qXgq', 'https://api.fathoor.dev/v1/file/img/default.png', 1337);
