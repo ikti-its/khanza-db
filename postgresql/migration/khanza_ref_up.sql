f346a15f1834995267af3d20f1d64de204b9f0d1
leonardywt <leonardy.theo@gmail.com>
2024-04-03 15:14:33 +0700

    modul_d

--- postgresql/migration/khanza_ref_up.sql
+++ postgresql/migration/khanza_ref_up.sql
@@ -57,4 +57,83 @@ CREATE TABLE IF NOT EXISTS alasan_cuti (
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
--- Modul D (Leon)
+-- MODUL D (Leo)
+-- Industri Farmasi
+CREATE TABLE IF NOT EXISTS industri_farmasi (
+    id INT PRIMARY KEY,
+    kode VARCHAR(20) NOT NULL UNIQUE,
+    nama VARCHAR(50) NOT NULL,
+    alamat VARCHAR(255) NOT NULL,
+    kota VARCHAR(255) NOT NULL,
+    telepon VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO industri_farmasi (id, kode, nama, alamat, kota, telepon) VALUES ('1000', 'KLBF', 'Kalbe Farma', 'Jln. jalan', 'Jakarta','0812312312');
+
+-- Jenis Obat
+CREATE TABLE IF NOT EXISTS ref.jenis_obat (
+    id INT PRIMARY KEY,
+    nama VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO ref.jenis_obat (id, nama) VALUES ('1000', 'Obat Oral'), ('2000', 'Obat Topikal'), ('3000', 'Obat Injeksi'), ('4000', 'Obat Sublingual'), ('5000', 'Obat Infus');
+
+-- Kategori Obat
+CREATE TABLE IF NOT EXISTS ref.kategori_obat (
+    id INT PRIMARY KEY,
+    nama VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO ref.kategori_obat (id, nama) VALUES ('1000', 'Obat Paten'), ('2000', 'Obat Generik'), ('3000', 'Obat Merek'), ('4000', 'Obat Eksklusif'), ('5000', 'Obat Bebas Paten');
+
+-- Golongan Obat
+CREATE TABLE IF NOT EXISTS ref.golongan_obat (
+    id INT PRIMARY KEY,
+    nama VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO ref.golongan_obat (id, nama) VALUES ('1000', 'Analgesik'), ('2000', 'Antibiotik'), ('3000', 'Antijamur'), ('4000', 'Antivirus'), ('5000', 'Antasida');
+
+-- Ruangan
+CREATE TABLE IF NOT EXISTS ref.ruangan (
+    id INT PRIMARY KEY,
+    nama VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO ref.ruangan (id, nama) VALUES ('1000', 'VIP 1'), ('2000', 'VIP 2'), ('3000', 'VVIP 1'), ('4000', 'VVIP 2'), ('5000', 'Gudang Farmasi');
+
+-- Supplier Barang Medis
+CREATE TABLE IF NOT EXISTS ref.supplier_barang_medis (
+    id INT PRIMARY KEY,
+    nama VARCHAR(50) NOT NULL,
+    alamat VARCHAR(255) NOT NULL,
+    no_telp VARCHAR(20) NOT NULL,
+    kota VARCHAR(50) NOT NULL,
+    nama_bank VARCHAR(100) NOT NULL,
+    no_rekening VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO ref.supplier_barang_medis (id, nama, alamat, no_telp, kota, nama_bank, no_rekening) VALUES ('1', 'Mitra', 'Jln. Benar', '08234234','Jakarta', 'BCA','8123123');
+
+-- Akun Bayar
+CREATE TABLE IF NOT EXISTS ref.akun_bayar (
+    id INT PRIMARY KEY,
+    nama VARCHAR(50) NOT NULL,
+    nomor_rekening VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO ref.akun_bayar (id, nama, nomor_rekening) VALUES ('1000', 'Cash', '-'), ('2000', 'Bank Mandiri', '12308123123'), ('3000', 'Bank BCA', '12208123123');
--- postgresql/migration/khanza_sik_up.sql
+++ postgresql/migration/khanza_sik_up.sql
@@ -249,4 +249,318 @@ CREATE INDEX IF NOT EXISTS idx_cuti_status ON cuti (status);
 CREATE INDEX IF NOT EXISTS idx_cuti_updated_at ON cuti (updated_at);
 CREATE INDEX IF NOT EXISTS idx_cuti_deleted_at ON cuti (deleted_at);
 
--- Modul D (Leon)
+-- Modul D (Leo)
+-- Barang Medis
+CREATE TYPE sik.jenis_barang_medis AS ENUM ('Obat', 'Alat Kesehatan', 'Bahan Habis Pakai', 'Darah');
+
+CREATE TABLE IF NOT EXISTS sik.barang_medis (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nama VARCHAR(100) NOT NULL,
+    jenis jenis_barang_medis NOT NULL,
+    harga FLOAT NOT NULL DEFAULT 0,
+    stok INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID,
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+    CREATE INDEX IF NOT EXISTS idx_barang_medis_jenis ON barang_medis (jenis);
+    CREATE INDEX IF NOT EXISTS idx_barang_medis_updated_at ON barang_medis (updated_at);
+    CREATE INDEX IF NOT EXISTS idx_barang_medis_deleted_at ON barang_medis (deleted_at);
+
+-- Obat
+CREATE TYPE sik.satuan_obat AS ENUM ('tablet', 'kapsul', 'ampul', 'botol', 'tube', 'vial', 'injeksi');
+
+CREATE TABLE IF NOT EXISTS sik.obat (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id_barang_medis UUID NOT NULL,
+    id_industri_farmasi INT NOT NULL,
+    kandungan VARCHAR(100) NOT NULL,
+    id_satuan_besar satuan_obat NOT NULL,
+    id_satuan_kecil satuan_obat NOT NULL,
+    isi INT NOT NULL,
+    kapasitas INT NOT NULL,
+    id_jenis INT NOT NULL,
+    id_kategori INT NOT NULL,
+    id_golongan INT NOT NULL,
+    kadaluwarsa DATE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID,
+    FOREIGN KEY (id_barang_medis) REFERENCES barang_medis (id),
+    FOREIGN KEY (id_industri_farmasi) REFERENCES ref.industri_farmasi (id),
+    FOREIGN KEY (id_jenis) REFERENCES ref.jenis_obat (id),
+    FOREIGN KEY (id_kategori) REFERENCES ref.kategori_obat (id),
+    FOREIGN KEY (id_golongan) REFERENCES ref.golongan_obat (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_obat_id_barang_medis ON obat (id_barang_medis);
+CREATE INDEX IF NOT EXISTS idx_obat_id_industri_farmasi ON obat (id_industri_farmasi);
+CREATE INDEX IF NOT EXISTS idx_obat_kadaluwarsa ON obat (kadaluwarsa);
+CREATE INDEX IF NOT EXISTS idx_obat_updated_at ON obat (updated_at);
+CREATE INDEX IF NOT EXISTS idx_obat_deleted_at ON obat (deleted_at);
+
+-- Alat Kesehatan
+CREATE TYPE merek_alat_kesehatan AS ENUM ('Omron','Philips', 'GE Healthcare', 'Siemens Healthineers', 'Medtronic', 'Johnson & Johnson', 'Becton, Dickinson and Company (BD)', 'Stryker', 'Boston Scientific', 'Olympus Corporation', 'Roche Diagnostics');
+
+CREATE TABLE IF NOT EXISTS sik.alat_kesehatan (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id_barang_medis UUID NOT NULL,
+    merek merek_alat_kesehatan NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID,
+    FOREIGN KEY (id_barang_medis) REFERENCES barang_medis (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_alat_kesehatan_merek ON alat_kesehatan (merek);
+CREATE INDEX IF NOT EXISTS idx_alat_kesehatan_updated_at ON alat_kesehatan (updated_at);
+CREATE INDEX IF NOT EXISTS idx_alat_kesehatan_deleted_at ON alat_kesehatan (deleted_at);
+
+-- Bahan Habis Pakai
+CREATE TYPE satuan_bahan_habis_pakai AS ENUM ('pasang', 'kotak', 'paket', 'item', 'botol', 'tabung', 'ml');
+
+CREATE TABLE IF NOT EXISTS sik.bahan_habis_pakai (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id_barang_medis UUID NOT NULL,
+    satuan satuan_bahan_habis_pakai_medis NOT NULL,
+    jumlah INT NOT NULL,
+    kadaluwarsa DATE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID,
+    FOREIGN KEY (id_barang_medis) REFERENCES barang_medis (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_bahan_habis_pakai_updated_at ON bahan_habis_pakai (updated_at);
+CREATE INDEX IF NOT EXISTS idx_bahan_habis_pakai_deleted_at ON bahan_habis_pakai (deleted_at);
+
+-- Darah
+CREATE TABLE IF NOT EXISTS sik.darah (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id_barang_medis UUID NOT NULL,
+    keterangan TEXT,
+    kadaluwarsa DATE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID,
+    FOREIGN KEY (id_barang_medis) REFERENCES barang_medis (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_darah_updated_at ON darah (updated_at);
+CREATE INDEX IF NOT EXISTS idx_darah_deleted_at ON darah (deleted_at);
+
+-- Pengajuan Barang Medis
+CREATE TYPE status_pesanan AS ENUM ('0', '1', '2', '3', '4', '5'); -- Menunggu persetujuan,  Pengajuan Ditolak, Pengajuan disetujui, Dalam pemesanan, Barang telah sampai, Tagihan telah dibayar
+
+CREATE TABLE IF NOT EXISTS sik.pengajuan_barang_medis (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    tanggal_pengajuan DATE DEFAULT CURRENT_DATE,
+    nomor_pengajuan VARCHAR(20) NOT NULL UNIQUE,
+    id_supplier INT NOT NULL,
+    id_pegawai UUID NOT NULL, 
+    diskon_persen FLOAT NOT NULL,
+    diskon_jumlah FLOAT NOT NULL,
+    pajak_persen FLOAT NOT NULL,
+    pajak_jumlah FLOAT NOT NULL,
+    materai FLOAT NOT NULL,
+    catatan VARCHAR(255),
+    status_pesanan status_pesanan NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID,
+    FOREIGN KEY (id_supplier) REFERENCES ref.supplier_barang_medis (id),
+    FOREIGN KEY (id_pegawai) REFERENCES pegawai (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+CREATE INDEX IF NOT EXISTS idx_pengajuan_barang_medis_tanggal_pengajuan ON pengajuan_barang_medis (tanggal_pengajuan);
+CREATE INDEX IF NOT EXISTS idx_pengajuan_barang_medis_id_supplier ON pengajuan_barang_medis (id_supplier);
+CREATE INDEX IF NOT EXISTS idx_pengajuan_barang_medis_id_pegawai ON pengajuan_barang_medis (id_pegawai);
+CREATE INDEX IF NOT EXISTS idx_pengajuan_barang_medis_updated_at ON pengajuan_barang_medis (updated_at);
+CREATE INDEX IF NOT EXISTS idx_pengajuan_barang_medis_deleted_at ON pengajuan_barang_medis (deleted_at);
+
+-- Persetujuan pengajuan
+CREATE TYPE status_persetujuan AS ENUM ('Disetujui', 'Ditolak');
+
+CREATE TABLE IF NOT EXISTS sik.persetujuan_pengajuan (
+    id_pengajuan UUID PRIMARY KEY,
+    status status_persetujuan NOT NULL,
+    status_apoteker status_persetujuan,
+    status_keuangan status_persetujuan,
+    id_apoteker UUID,
+    id_keuangan UUID,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID,
+    FOREIGN KEY (id_pengajuan) REFERENCES pengajuan_barang_medis (id),
+    FOREIGN KEY (id_apoteker) REFERENCES pegawai (id),
+    FOREIGN KEY (id_keuangan) REFERENCES pegawai (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_persetujuan_pengajuan_status_keuangan ON persetujuan_pengajuan (status_keuangan);
+CREATE INDEX IF NOT EXISTS idx_persetujuan_pengajuan_status_apoteker ON persetujuan_pengajuan (status_apoteker);
+CREATE INDEX IF NOT EXISTS idx_persetujuan_pengajuan_id_apoteker ON persetujuan_pengajuan (id_apoteker);
+CREATE INDEX IF NOT EXISTS idx_persetujuan_pengajuan_id_keuangan ON persetujuan_pengajuan (id_keuangan);
+CREATE INDEX IF NOT EXISTS idx_persetujuan_pengajuan_updated_at ON persetujuan_pengajuan (updated_at);
+CREATE INDEX IF NOT EXISTS idx_persetujuan_pengajuan_deleted_at ON persetujuan_pengajuan (deleted_at);
+
+CREATE OR REPLACE FUNCTION update_status_pengajuan()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.status_apoteker IS NULL OR NEW.status_keuangan IS NULL THEN
+        NEW.status := 'Menunggu Persetujuan';
+    ELSIF NEW.status_apoteker = 'Ditolak' OR NEW.status_keuangan = 'Ditolak' THEN
+        NEW.status := 'Ditolak';
+    ELSIF NEW.status_apoteker = 'Disetujui' AND NEW.status_keuangan = 'Disetujui' THEN
+        NEW.status := 'Disetujui';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_status_pengajuan
+BEFORE INSERT OR UPDATE OF status_apoteker, status_keuangan ON persetujuan_pengajuan
+FOR EACH ROW
+EXECUTE FUNCTION update_status_pengajuan();
+
+-- Pesanan Barang Medis
+CREATE TABLE IF NOT EXISTS sik.pesanan_barang_medis (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id_pengajuan UUID NOT NULL,
+    id_barang_medis UUID NOT NULL,
+    harga_satuan FLOAT NOT NULL,
+    jumlah_pesanan INT NOT NULL,
+    jumlah_diterima INT NOT NULL DEFAULT 0,
+    kadaluwarsa DATE NOT NULL,
+    no_batch VARCHAR(20),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, 
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, 
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID, 
+    FOREIGN KEY (id_pengajuan) REFERENCES pengajuan_barang_medis (id),
+    FOREIGN KEY (id_barang_medis) REFERENCES barang_medis (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_pesanan_barang_medis_id_pengajuan ON pesanan_barang_medis (id_pengajuan);
+CREATE INDEX IF NOT EXISTS idx_pesanan_barang_medis_id_barang_medis ON pesanan_barang_medis (id_barang_medis);
+CREATE INDEX IF NOT EXISTS idx_pesanan_barang_medis_updated_at ON pesanan_barang_medis (updated_at);
+CREATE INDEX IF NOT EXISTS idx_pesanan_barang_medis_deleted_at ON pesanan_barang_medis (deleted_at);
+
+
+-- Pemesanan Barang Medis
+CREATE TABLE IF NOT EXISTS sik.pemesanan_barang_medis (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    tanggal_pesan DATE NOT NULL,
+    no_pemesanan VARCHAR(20) NOT NULL,
+    id_pengajuan UUID NOT NULL,
+    id_pegawai UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID, 
+    FOREIGN KEY (id_pengajuan) REFERENCES pengajuan_barang_medis (id),
+    FOREIGN KEY (id_pegawai) REFERENCES pegawai (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_pemesanan_barang_medis_id_pengajuan ON pemesanan_barang_medis (id_pengajuan);
+CREATE INDEX IF NOT EXISTS idx_pemesanan_barang_medis_id_pegawai ON pemesanan_barang_medis (id_pegawai);
+CREATE INDEX IF NOT EXISTS idx_pemesanan_barang_medis_id_updated_at ON pemesanan_barang_medis (updated_at);
+CREATE INDEX IF NOT EXISTS idx_pemesanan_barang_medis_id_deleted_at ON pemesanan_barang_medis (deleted_at);
+
+-- Penerimaan Barang Medis
+CREATE TABLE IF NOT EXISTS sik.penerimaan_barang_medis (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id_pengajuan UUID NOT NULL,
+    id_pemesanan UUID NOT NULL,
+    no_faktur VARCHAR(20) NOT NULL,
+    tanggal_datang DATE NOT NULL,
+    tanggal_faktur DATE NOT NULL,
+    tanggal_jthtempo DATE NOT NULL,
+    id_pegawai UUID NOT NULL, 
+    id_ruangan INT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID, 
+    FOREIGN KEY (id_pengajuan) REFERENCES pengajuan_barang_medis (id),
+    FOREIGN KEY (id_pemesanan) REFERENCES pemesanan_barang_medis (id),
+    FOREIGN KEY (id_pegawai) REFERENCES pegawai (id),
+    FOREIGN KEY (id_ruangan) REFERENCES ref.ruangan (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_penerimaan_barang_medis_id_pengajuan ON penerimaan_barang_medis (id_pengajuan);
+CREATE INDEX IF NOT EXISTS idx_penerimaan_barang_medis_id_pemesanan ON penerimaan_barang_medis (id_pemesanan);
+CREATE INDEX IF NOT EXISTS idx_penerimaan_barang_medis_id_ruangan ON penerimaan_barang_medis (id_ruangan);
+CREATE INDEX IF NOT EXISTS idx_penerimaan_barang_medis_id_pegawai ON penerimaan_barang_medis (id_pegawai);
+CREATE INDEX IF NOT EXISTS idx_penerimaan_barang_medis_updated_at ON penerimaan_barang_medis (updated_at);
+CREATE INDEX IF NOT EXISTS idx_penerimaan_barang_medis_deleted_at ON penerimaan_barang_medis (deleted_at);
+
+-- Bayar Tagihan Barang Medis
+CREATE TABLE IF NOT EXISTS sik.tagihan_barang_medis (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id_pengajuan UUID NOT NULL,
+    id_pemesanan UUID NOT NULL,
+    id_penerimaan UUID NOT NULL,
+    tanggal_bayar DATE NOT NULL,
+    jumlah_bayar FLOAT NOT NULL,
+    id_pegawai UUID NOT NULL, 
+    keterangan VARCHAR(255),
+    no_bukti VARCHAR(50) NOT NULL,
+    id_akun_bayar INT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID, 
+    FOREIGN KEY (id_pengajuan) REFERENCES pengajuan_barang_medis (id),
+    FOREIGN KEY (id_pemesanan) REFERENCES pemesanan_barang_medis (id),
+    FOREIGN KEY (id_penerimaan) REFERENCES penerimaan_barang_medis (id),
+    FOREIGN KEY (id_pegawai) REFERENCES pegawai (id),
+    FOREIGN KEY (id_akun_bayar) REFERENCES ref.akun_bayar (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_tagihan_barang_medis_id_pengajuan ON tagihan_barang_medis (id_pengajuan);
+CREATE INDEX IF NOT EXISTS idx_tagihan_barang_medis_id_pemesanan ON tagihan_barang_medis (id_pemesanan);
+CREATE INDEX IF NOT EXISTS idx_tagihan_barang_medis_id_penerimaan ON tagihan_barang_medis (id_penerimaan);
+CREATE INDEX IF NOT EXISTS idx_tagihan_barang_medis_tanggal_bayar ON tagihan_barang_medis (tanggal_bayar);
+CREATE INDEX IF NOT EXISTS idx_tagihan_barang_medis_id_pegawai ON tagihan_barang_medis (id_pegawai);
+CREATE INDEX IF NOT EXISTS idx_tagihan_barang_medis_id_akun_bayar ON tagihan_barang_medis (id_akun_bayar);
+CREATE INDEX IF NOT EXISTS idx_tagihan_barang_medis_updated_at ON tagihan_barang_medis (updated_at);
+CREATE INDEX IF NOT EXISTS idx_tagihan_barang_medis_deleted_at ON tagihan_barang_medis (deleted_at);
+
+-- Stok Keluar Barang Medis
+CREATE TABLE IF NOT EXISTS sik.stok_keluar_barang_medis (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    no_keluar VARCHAR(20) NOT NULL,
+    id_barang_medis UUID NOT NULL,
+    id_pegawai UUID NOT NULL, 
+    tanggal_stok_keluar DATE NOT NULL DEFAULT CURRENT_DATE,
+    jumlah_keluar INT NOT NULL,
+    keterangan VARCHAR(255), 
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID, 
+    FOREIGN KEY (id_pegawai) REFERENCES pegawai (id),
+    FOREIGN KEY (updater) REFERENCES akun (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_stok_keluar_barang_medis_tanggal_stok_keluar ON stok_keluar_barang_medis (tanggal_stok_keluar);
+CREATE INDEX IF NOT EXISTS idx_stok_keluar_barang_medis_id_pegawai ON stok_keluar_barang_medis (id_pegawai);
+CREATE INDEX IF NOT EXISTS idx_stok_keluar_barang_medis_updated_at ON stok_keluar_barang_medis (updated_at);
+CREATE INDEX IF NOT EXISTS idx_stok_keluar_barang_medis_deleted_at ON stok_keluar_barang_medis (deleted_at);
