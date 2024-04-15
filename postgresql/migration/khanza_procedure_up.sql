3d3d03ab3734ae49d9188ebad8c086f377d77807
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-16 00:58:08 +0700

    chore: separate function or procedure DDLs

---
+++ postgresql/migration/khanza_procedure_up.sql
@@ -0,0 +1,67 @@
+-- Modul C (Fathur, Ruben)
+-- Jadwal Pegawai
+CREATE OR REPLACE FUNCTION default_jadwal_pegawai(IN new_id UUID)
+RETURNS VOID AS $$
+BEGIN
+    INSERT INTO jadwal_pegawai (id_pegawai, id_hari, id_shift)
+    SELECT new_id, hari.id, 'NA'
+    FROM ref.hari;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION trigger_init_jadwal_pegawai()
+RETURNS TRIGGER AS $$
+BEGIN
+    PERFORM default_jadwal_pegawai(NEW.id);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION update_jadwal_pegawai_on_delete(IN pegawai_id UUID)
+RETURNS VOID AS $$
+BEGIN
+  UPDATE jadwal_pegawai
+  SET deleted_at = CURRENT_TIMESTAMP
+  WHERE id_pegawai = pegawai_id;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION trigger_update_jadwal_pegawai_on_delete()
+RETURNS TRIGGER AS $$
+BEGIN
+    PERFORM update_jadwal_pegawai_on_delete(OLD.id);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER init_jadwal_pegawai
+AFTER INSERT ON pegawai
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_init_jadwal_pegawai();
+
+CREATE TRIGGER update_jadwal_pegawai_on_delete
+BEFORE UPDATE ON pegawai
+FOR EACH ROW
+WHEN (OLD.deleted_at IS NULL AND NEW.deleted_at IS NOT NULL)
+EXECUTE FUNCTION trigger_update_jadwal_pegawai_on_delete();
+
+-- Modul D (Leo)
+-- Persetujuan pengajuan
+CREATE OR REPLACE FUNCTION update_status_pengajuan()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.status_apoteker IS NULL OR NEW.status_keuangan IS NULL THEN
+        NEW.status := 'Menunggu Persetujuan';
+    ELSIF NEW.status_apoteker = 'Ditolak' OR NEW.status_keuangan = 'Ditolak' THEN
+        NEW.status := 'Ditolak';
+    ELSIF NEW.status_apoteker = 'Disetujui' AND NEW.status_keuangan = 'Disetujui' THEN
+        NEW.status := 'Disetujui';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_status_pengajuan
+BEFORE INSERT OR UPDATE OF status_apoteker, status_keuangan ON persetujuan_pengajuan
+FOR EACH ROW
+EXECUTE FUNCTION update_status_pengajuan();
