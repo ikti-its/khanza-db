7b4eff95ee8a65d24e3d821654016a7182d6bc66
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-17 23:23:38 +0700

    chore: init metabase DDL

---
+++ postgresql/migration/metabase_app_up.sql
@@ -0,0 +1,13 @@
+-- Akun
+CREATE TABLE IF NOT EXISTS akun (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    foto VARCHAR(255) NOT NULL,
+    role INT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updater UUID,
+    FOREIGN KEY (role) REFERENCES ref.role (id)
+);
---
+++ postgresql/seed/metabase_ref_seed.sql
@@ -0,0 +1,2 @@
+-- Role
+INSERT INTO role (id, nama) VALUES (1, 'Admin'), (2, 'Pegawai'), (1337, 'Developer');
